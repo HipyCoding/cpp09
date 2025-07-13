ffb3eeaed1a0e2a3b821b561f2316ddb11c032af
Candrese <candrese@student.42Heilbronn.de>
2025-07-13 18:05:25 +0200

    start of pmergme template class

--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -6,7 +6,22 @@
 /*   By: candrese <candrese@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/13 17:27:39 by candrese          #+#    #+#             */
-/*   Updated: 2025/07/13 17:27:40 by candrese         ###   ########.fr       */
+/*   Updated: 2025/07/13 18:04:11 by candrese         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+template <typename TContainer>
+class PmergeMe
+{
+	private:
+		//containers
+	public:
+		PmergeMe(void) =  delete;
+		PmergeMe(char **argv);
+		PmergeMe(const PmergeMe& src)=  delete;
+		PmergeMe &operator=(const PmergeMe &src)=  delete;
+		~PmergeMe() = default;
+
+	//some getters and setters
+	//exceptions
+}
