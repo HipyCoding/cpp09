142358cdde60123f46b59f8a621152b27b7d7541
Candrese <candrese@student.42Heilbronn.de>
2025-07-15 15:53:30 +0200

    with that parsing is fully finished in ex00. aso added test file

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,31 @@
 /*   By: candrese <candrese@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/13 16:58:05 by candrese          #+#    #+#             */
-/*   Updated: 2025/07/13 16:58:06 by candrese         ###   ########.fr       */
+/*   Updated: 2025/07/15 15:00:33 by candrese         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "BitcoinExchange.hpp"
+
+int main(int argc, char* argv[])
+{
+	if (argc != 2)
+	{
+		std::cerr << "Usage: ./btc <input_file>" << std::endl;
+		std::cerr << "error: could not open file." << std::endl;
+		return 1;
+	}
+	
+	try
+	{
+		BitcoinExchange bitcoinCalculator;
+		bitcoinCalculator.processFile(argv[1]);
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+		return 1;
+	}
+	
+	return 0;
+}
